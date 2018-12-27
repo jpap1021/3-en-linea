643ee1a8291db25afbb044e1952070062d089706
Juan Pablo <juan>
2018-12-27 14:13:27 -0500

    esqueleto triqui

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-class Game extends Component {
-    render(){
-        return (
-        <h1>hola react</h1>
-        )
-    }
-}
+import Game from './App';
 
-render(<Game/>,document.getElementById('app'))
+
+render(<Game />, document.getElementById("app"));
+  
+  
